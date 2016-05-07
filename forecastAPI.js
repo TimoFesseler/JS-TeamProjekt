f774dd78e6cc73f5be36738c2e0b0a61bacd6686
Fabian Tschullik <ft027@hdm-stuttgart.de>
2016-05-07 22:28:13 +0200

    Forecast API ausgelesen und in MongoDB gespeichert

---
+++ forecastAPI.js
@@ -0,0 +1,129 @@
+var request = require('request');
+
+var mongodb = require('mongodb');
+var assert = require('assert');
+
+var MongoClient = mongodb.MongoClient;
+var url = 'mongodb://localhost/test';
+
+var data = null;
+var body = null;
+
+
+MongoClient.connect(url, function (err, db) {
+    if (err) {
+        console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+        //HURRAY!! We are connected. :)
+        console.log('Connection established to', url);
+
+
+
+
+
+request('http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=1ccf6bfbf52e4b3abadde9b4125547d3', function (error, response, body) {
+    if (!error) {
+
+
+        data = JSON.parse(body);
+
+        //console.log(data);
+
+        console.log('Stadt: '+data.city.name);
+        console.log('Stadt ID: '+data.city.id);
+        console.log('Längengrad: '+data.city.coord.lon);
+        console.log('Breitengrad: '+data.city.coord.lat);
+
+        console.log('Datum: '+data.list[0].dt_txt);
+        console.log('Temperatur: '+data.list[0].main.temp);
+        console.log('Wolken '+data.list[0].clouds.all);
+        console.log('Länge-Liste: '+data.list.length);
+
+
+
+
+
+
+
+        var forecast = {    'city_id' : data.city.id,
+                            'city_name': data.city.name,
+                            'cords' : {'lon' : data.city.coord.lon, 'lat' : data.city.coord.lat},
+                            'forecast' : []
+
+        };
+
+
+
+        for (var i = 0; i < data.list.length; i++) {
+
+
+            forecast.forecast.push({
+
+                'date_time': data.list[i].dt_txt, 'temp': +data.list[i].main.temp, 'clouds' : data.list[i].clouds.all
+
+            });
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(forecast);
+
+
+
+
+        var collection = db.collection('forecast');
+        collection.insert([forecast], function (err, result) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Erfolg');
+            }
+            //Close connection
+            db.close();
+        });
+
+
+
+
+
+    }
+
+    //console.log(error);
+
+});
+
+
+
+
+
+// Use connect method to connect to the Server
+
+
+
+
+
+// Insert some users
+
+
+
+
+
+
+
+
+
+
+    }
+});
+
+
