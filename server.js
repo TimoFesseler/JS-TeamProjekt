56121eca1442df7cd11487cd8383795c6be45e67
Fabian Tschullik <ft027@hdm-stuttgart.de>
2016-06-08 23:40:47 +0200

    no message

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
+/**
+ * Created by Fabian Tschullik on 08.06.2016.
+ */
+
+
 var express = require('express')
 ,   app = express()
 ,   server = require('http').createServer(app)
 ,   io = require('socket.io').listen(server)
 ,   conf = require('./config.json');
 
-var mongoose = require('mongoose');
-
-
-
 // Webserver
 // auf den Port x schalten
 server.listen(conf.port);
 
-
+app.configure(function(){
 	// statische Dateien ausliefern
 	app.use(express.static(__dirname + '/public'));
-
+});
 
 // wenn der Pfad / aufgerufen wird
 app.get('/', function (req, res) {
@@ -25,58 +26,13 @@ app.get('/', function (req, res) {
 
 // Websocket
 io.sockets.on('connection', function (socket) {
-
-
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function(){
-  console.log("Connected to DBll");
-
-
-
-
-// Weather Model
-var weatherSchema = mongoose.Schema({
-
-
-      date_time : Date,
-      city_id : Number,
-      city_name : String,
-      cords : {
-          lon : Number,
-          lat : Number
-      },
-      sunrise : Number,
-      sunset :  Number,
-
-              temp : Number,
-              temp_min : Number,
-              temp_max : Number,
-              rain : String,
-              clouds : Number
-
-
-  });
-
-
-  var Weather = mongoose.model('weather', weatherSchema);
-
-
-Weather.find(function (err, weathers) {
-  if (err) return console.error(err);
-console.log(weathers);
-})
-
-
+	// der Client ist verbunden
 	socket.emit('chat', { zeit: new Date(), text: 'Du bist nun mit dem Server verbunden!' });
-
-
-});
-
-
-
-
+	// wenn ein Benutzer einen Text senden
+	socket.on('chat', function (data) {
+		// so wird dieser Text an alle anderen Benutzer gesendet
+		io.sockets.emit('chat', { zeit: new Date(), name: data.name || 'Anonym', text: data.text });
+	});
 });
 
 // Portnummer in die Konsole schreiben
