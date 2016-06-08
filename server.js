14c407549f51e3ec3bd7cdb69a4fa77cd4546d5a
Fabian Tschullik <ft027@hdm-stuttgart.de>
2016-06-08 16:59:42 +0200

    websoccet test

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server)
 
-app.use(express.static('public'));
 
+// Webserver
+// auf den Port x schalten
+server.listen(8081);
+
+
+app.configure(function(){
+	// statische Dateien ausliefern
+	app.use(express.static(__dirname + '/public'));
+});
+
+
+// wenn der Pfad / aufgerufen wird
 app.get('/', function (req, res) {
-    res.send('Hello World');
-})
+	// so wird die Datei index.html ausgegeben
+	res.sendfile(__dirname + '/public/index.html');
+});
 
-var server = app.listen(8081, function () {
 
-    var host = server.address().address
-    var port = server.address().port
+// Websocket
+io.sockets.on('connection', function (socket) {
+	// der Client ist verbunden
+	socket.emit('chat', { zeit: new Date(), text: 'Du bist nun mit dem Server verbunden!' });
 
-    console.log("Example app listening at http://%s:%s", host, port)
+	});
+});
 
-})
+// Portnummer in die Konsole schreiben
+console.log('Der Server läuft nun unter http://127.0.0.1:' + conf.port + '/');
+
 
