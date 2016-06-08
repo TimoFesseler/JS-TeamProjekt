68a7fc33ce9b8406f5d72faac9488c1c2edb3d8d
Fabian Tschullik <ft027@hdm-stuttgart.de>
2016-06-08 17:03:53 +0200

    no message

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app)
-var io = require('socket.io').listen(server)
-
+var express = require('express')
+,   app = express()
+,   server = require('http').createServer(app)
+,   io = require('socket.io').listen(server)
+,   conf = require('./config.json');
 
 // Webserver
 // auf den Port x schalten
-server.listen(8081);
-
+server.listen(conf.port);
 
 app.configure(function(){
 	// statische Dateien ausliefern
 	app.use(express.static(__dirname + '/public'));
 });
-
 
 // wenn der Pfad / aufgerufen wird
 app.get('/', function (req, res) {
@@ -21,15 +19,16 @@ app.get('/', function (req, res) {
 	res.sendfile(__dirname + '/public/index.html');
 });
 
-
 // Websocket
 io.sockets.on('connection', function (socket) {
 	// der Client ist verbunden
 	socket.emit('chat', { zeit: new Date(), text: 'Du bist nun mit dem Server verbunden!' });
-
+	// wenn ein Benutzer einen Text senden
+	socket.on('chat', function (data) {
+		// so wird dieser Text an alle anderen Benutzer gesendet
+		io.sockets.emit('chat', { zeit: new Date(), name: data.name || 'Anonym', text: data.text });
 	});
+});
 
 // Portnummer in die Konsole schreiben
 console.log('Der Server läuft nun unter http://127.0.0.1:' + conf.port + '/');
-
-
