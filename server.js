643c93ae48c41f4590ebb9025b4c29196c27637e
TimoFesseler <timo.fesseler@gmail.com>
2016-06-09 00:33:56 +0200

    diesmal mit Dateien

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ io.sockets.on('connection', function (socket) {
 	// wenn ein Benutzer einen Text senden
 	socket.on('chat', function (data) {
 		// so wird dieser Text an alle anderen Benutzer gesendet
-		io.sockets.emit('chat', { zeit: new Date(), name: data.name || 'Anonym', text: data.text });
+		io.sockets.emit('chat', { zeit: new Date(), name: data.name || 'Anonym', text: data[1].text });
 	});
 });
 
