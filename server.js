0b9f4dc88689a0b72aa1119fbea3298f1916b6cc
Fabian Tschullik <fabiantschullik@Fabians-MacBook-Pro.local>
2016-06-09 16:38:02 +0200

    no message

--- server.js
+++ server.js
@@ -50,9 +50,8 @@ db.once('open', function(){
     var Weather = mongoose.model('weather', weatherSchema);
 
 
-    var q = Weather.find({published: true}).sort({'date': -1}).limit(20);
+    var q = Weather.find().sort({'date': -1}).limit(20);
     q.exec(function(err, posts) {
-console.log(posts.stringify());
 
     });
 
