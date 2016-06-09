86a51105bac9fceb96bc60f2475a692e158cc4ff
Fabian Tschullik <fabiantschullik@Fabians-MacBook-Pro.local>
2016-06-09 16:59:43 +0200

    no message

--- public/js/energyForecast.js
+++ public/js/energyForecast.js
@@ -24,7 +24,7 @@ var yAxis = d3.svg.axis()
     .orient("left")
     .ticks(10, "%");
 
-var svg = d3.select("blajkjkj").append("svg")
+var svg = d3.select("bla").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
